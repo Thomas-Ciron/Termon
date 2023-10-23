1570206421eca436b44af389c045e82eecfa1109
Thomas-Ciron <thomas.ciron@outlook.com>
2023-10-23 15:19:56 +0200

    fix: help display

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 **Termon** is a software that allows the user to play Pokémon on terminal. 
 
 My goal is to train myself to code in C with using as few computing resources as possible. 
+
+## Executable options
+Usage : `termon [OPTION]...`
+Play Pokemon on terminal.
+
+With no OPTION, play Pokemon.
+- `-l` or `--language` : set the language
+- `-h` or `--help` : display help and exit
+- `-v` or `--version` : display version information and exit
--- main.c
+++ main.c
@@ -2,18 +2,22 @@
 #include <string.h>
 #include "settings.h"
 
-const char* VERSION = "1.00";
+const char* VERSION = "1.01";
 
 void help() {
     printf("TERMON\n\n");
     printf("Usage: termon [OPTION]...\n");
     printf("Play Pokemon on terminal.\n\n");
 
-    printf("With no OPTION, play Pokémon.\n\n");
+    printf("With no OPTION, play Pokemon.\n\n");
 
-    printf("\t-l, --language\tset the language\n");
-    printf("\t-h, --help\tdisplay this help and exit\n");
-    printf("\t-v, --version\toutput version information and exit\n");
+    printf("    -l, --language\tset the language and exit\n");
+    printf("    -h, --help\t\tdisplay this help and exit\n");
+    printf("    -v, --version\toutput version information and exit\n");
+
+    printf("Examples :\n");
+    printf("    termon\t\tPlay Pokemon\n");
+    printf("    termon -l en\tSwitch language to English\n");
 }
 
 void main(int argc, char** argv) {
@@ -22,7 +26,7 @@ void main(int argc, char** argv) {
     }
     else if(argc == 2) {
         if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) help();
-        if(strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) printf("Termon %s", VERSION);
+        else if(strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) printf("Termon %s", VERSION);
         else printf("Error 000 : unknown command.");
     }
     else if(argc == 3) {
